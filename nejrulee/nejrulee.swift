33db34b77be51704f2630112923747bcaf798161
mini3 <mini3@mini3s-Mac-mini.local>
2017-03-31 10:23:10 +0200

    nesto

---
+++ nejrulee/nejrulee.swift
@@ -0,0 +1,18 @@
+//
+//  nejrulee.swift
+//  nejrulee
+//
+//  Created by mini3 on 31/03/17.
+//  Copyright © 2017 mini3. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class Nejra: UIViewController {
+    @IBOutlet var playLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.playLabel.text="Stagod"
+    }
+    
+}
